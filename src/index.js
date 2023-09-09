c7d881939ee6a38b584114f5f353e84b29f44cf6
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-09-09 22:59:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.scss';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/project05_frontend_test">
+      <BrowserRouter basename="/react_node_fs72_Pavlo-V-M">
         <App />
       </BrowserRouter>
     </Provider>
